4d27178f504c833a8d2d10d2874b48191dcd1f6c
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2019-04-07 14:15:14 -0300

    Added get_all_data_file signature

--- inc/handle_file.h
+++ inc/handle_file.h
@@ -1,6 +1,6 @@
 #ifndef HANDLE_FILE_H
     #define HANDLE_FILE_H
-    
+
         typedef struct t_file_header
         {
             long int topo_lista;
@@ -10,5 +10,6 @@
 
         void init_file_header(FILE_HEADER *header);
         void print_file_header(FILE_HEADER header);
+        void get_all_data_file(const char *file_name);
         void create_bin_file(const char *data_file_name, const char *csv_file_name);
 #endif
