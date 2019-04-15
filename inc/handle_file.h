1846a299295ca5679def1487f13bae449262b8f8
Paulo Ricardo Jordao Miranda <pauloricardojm@hotmail.com.br>
2019-04-15 16:14:01 -0300

    FILE_HEADER type removed

--- inc/handle_file.h
+++ inc/handle_file.h
@@ -1,13 +1,6 @@
 #ifndef HANDLE_FILE_H
     #define HANDLE_FILE_H
 
-        typedef struct t_file_header
-        {
-            long int topo_lista;
-            char desc_campo1[40], desc_campo2[40], desc_campo3[40], desc_campo4[40], desc_campo5[40];
-            char status, tag_campo1, tag_campo2, tag_campo3, tag_campo4, tag_campo5;
-        } FILE_HEADER;
-
         void get_all_data_file(const char *file_name);
         void create_bin_file(const char *data_file_name, const char *csv_file_name);
 #endif
