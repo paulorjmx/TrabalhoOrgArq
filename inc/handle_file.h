94c77fa90e8bfa6864bbad9f2f1bdaf7c10fcdcf
Paulo Ricardo Jordao Miranda <pauloricardojm@hotmail.com.br>
2019-04-16 17:44:38 -0300

    Added searches functions

--- inc/handle_file.h
+++ inc/handle_file.h
@@ -1,6 +1,11 @@
 #ifndef HANDLE_FILE_H
     #define HANDLE_FILE_H
 
-        void get_all_data_file(const char *file_name);
         void create_bin_file(const char *data_file_name, const char *csv_file_name);
+        void get_all_data_file(const char *file_name);
+        void search_for_id(const char *file_name, int id);
+        void search_for_salario(const char *file_name, double salario);
+        void search_for_telefone(const char *file_name, const char *telefone);
+        void search_for_nome(const char *file_name, const char *nome);
+        void search_for_cargo(const char *file_name, const char *cargo);
 #endif
