9dd3df85381d2c1d41f5bd116543869f37901a83
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2019-05-01 13:36:58 -0300

    Auxiliary functions

---
+++ inc/func_aux.h
@@ -0,0 +1,8 @@
+#ifndef H_ESCREVERNATELA_
+    #define H_ESCREVERNATELA_
+    
+        void binarioNaTela1(FILE *ponteiroArquivoBinario);
+        void binarioNaTela2(char *nomeArquivoBinario);
+        void trim(char *str);
+        void scan_quote_string(char *str);
+#endif
