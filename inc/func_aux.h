04e642d6da47d9ea8e9dc2b9478b1eda04b81dec
Paulo Ricardo <pauloricardojm@hotmail.com.br>
2019-05-01 19:41:31 -0300

    Added include stdio

--- inc/func_aux.h
+++ inc/func_aux.h
@@ -1,6 +1,7 @@
 #ifndef H_ESCREVERNATELA_
     #define H_ESCREVERNATELA_
-    
+        #include <stdio.h>
+
         void binarioNaTela1(FILE *ponteiroArquivoBinario);
         void binarioNaTela2(char *nomeArquivoBinario);
         void trim(char *str);
