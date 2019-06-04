4980db42f48c8a3a29d3a92817ac7746f2fd9096
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2019-06-04 15:40:02 -0300

    Header of file index

---
+++ inc/handle_index.h
@@ -0,0 +1,12 @@
+/* ##########################################
+#                                           #
+#   Autor: Paulo Ricardo Jordao Miranda     #
+#   N USP: 10133456                         #
+#                                           #
+###########################################*/
+
+#ifndef HANDLE_INDEX_H
+    #define HANDLE_INDEX_H
+
+        void create_index_file(const char *file_name, const char *data_file_name); // Funcao utilizada para criar o arquivo de indice referente ao arquivo 'data_file_name' 
+#endif
