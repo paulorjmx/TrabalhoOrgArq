25d421d44bf9c5a4df6c9d082482a2132d16d9eb
Paulo Ricardo J. Miranda <pauloricardojm@hotmail.com.br>
2019-06-04 16:44:06 -0300

    Added search_name_index sign

--- inc/handle_index.h
+++ inc/handle_index.h
@@ -8,5 +8,6 @@
 #ifndef HANDLE_INDEX_H
     #define HANDLE_INDEX_H
 
-        void create_index_file(const char *file_name, const char *data_file_name); // Funcao utilizada para criar o arquivo de indice referente ao arquivo 'data_file_name' 
+        void create_index_file(const char *file_name, const char *data_file_name); // Funcao utilizada para criar o arquivo de indice referente ao arquivo 'data_file_name'
+        long int search_name_index(const char *file_name, const char *nome); // Funcao utilizada para buscar um nome e retornar o byte offset do registro onde se encontra
 #endif
