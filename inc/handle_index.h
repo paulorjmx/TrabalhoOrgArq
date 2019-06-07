d87929ab691c9651a15c7515149dbabf98bb068a
Paulo Ricardo J Miranda <pauloricardojm@hotmail.com.br>
2019-06-07 18:33:19 -0300

    Added search name in index file - function sig

--- inc/handle_index.h
+++ inc/handle_index.h
@@ -9,5 +9,5 @@
     #define HANDLE_INDEX_H
 
         void create_index_file(const char *file_name, const char *data_file_name); // Funcao utilizada para criar o arquivo de indice referente ao arquivo 'data_file_name'
-        long int search_name_index(const char *file_name, const char *nome); // Funcao utilizada para buscar um nome e retornar o byte offset do registro onde se encontra
+        long int *search_name_index(const char *file_name, const char *nome, int *items_finded); // Funcao utilizada para buscar um nome e retornar um conjunto de bytes offset dos registros encontrados
 #endif
