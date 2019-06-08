0406b80a3f4a02f4a9066d29c434aba5d7ac35c1
Paulo Ricardo J Miranda <pauloricardojm@hotmail.com.br>
2019-06-08 19:41:26 -0300

    Added insert function sig

--- inc/handle_index.h
+++ inc/handle_index.h
@@ -13,5 +13,6 @@
         void create_index_file(const char *file_name, const char *data_file_name); // Funcao utilizada para criar o arquivo de indice referente ao arquivo 'data_file_name'
         long int *search_name_index(const char *file_name, const char *nome, int *items_finded); // Funcao utilizada para buscar um nome e retornar um conjunto de bytes offset dos registros encontrados
         int remove_index_file(const char *file_name, INDEX_DATA *data, unsigned int nitems, const char *nome); // Funcao utilizada para remover um registro do arquivo de indice
+        int insert_index_file(const char *file_name, INDEX_DATA **data, unsigned int *nitems, const char *nome, long int byte_offset); // Funcao utilizada para inserir um novo registro de indice
         void write_index_data(const char *file_name, INDEX_DATA *data, unsigned int nitems); // Funcao utilizada para escrever os registros no arquivo de indice
 #endif
